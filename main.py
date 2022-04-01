72707b73bedd889e15aec9f779ce1740c8998ac8
Mahsa <Sushcheva2009@yandex.ru>
2022-04-01 23:59:51 +0300

    Изучаю ботов

--- main.py
+++ main.py
@@ -1,6 +1,78 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
+import logging
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CommandHandler
+from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardRemove
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+TOKEN = '5146192395:AAFuwm-__5uK-tG7tfirvAWXCfo97idma54'
+
+# Напишем соответствующие функции.
+# Их сигнатура и поведение аналогичны обработчикам текстовых сообщений.
+# Напишем соответствующие функции.
+def start(update, context):
+    reply_keyboard = [['/address', '/phone'],
+                      ['/site', '/work_time'],
+                      ['/close']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+    update.message.reply_text(
+        "Я бот-справочник. Какая информация вам нужна?",
+        reply_markup=markup
+    )
+
+def close_keyboard(update, context):
+    update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+def help(update, context):
+    update.message.reply_text(
+        "Я бот справочник.")
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+def address(update, context):
+    update.message.reply_text(
+        "Адрес: г. Москва, ул. Льва Толстого, 16")
 
 
-server_address = ("", 8000)
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
+def phone(update, context):
+    update.message.reply_text("Телефон: +7(495)776-3030")
+
+
+def site(update, context):
+    update.message.reply_text(
+        "Сайт: http://www.yandex.ru/company")
+
+
+def work_time(update, context):
+    update.message.reply_text(
+        "Время работы: круглосуточно.")
+
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("address", address))
+    dp.add_handler(CommandHandler("phone", phone))
+    dp.add_handler(CommandHandler("site", site))
+    dp.add_handler(CommandHandler("work_time", work_time))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("close", close_keyboard))
+    updater.start_polling()
+
+    updater.idle()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
