29aebd1e8f9f3504cd596282ca7ab249cc01ac17
Mahsa <Sushcheva2009@yandex.ru>
2022-04-08 00:51:38 +0300

    Поиск через query почти доделан

--- main.py
+++ main.py
@@ -56,6 +56,9 @@ def site(update, context):
     update.message.reply_text(
         "Сайт: http://www.yandex.ru/company")
 
+def param(update, context):
+    context.user_data['dan'] = update.message.text
+    search_by_name(update, context)
 
 def button(update, _):
     query = update.callback_query
@@ -66,14 +69,12 @@ def button(update, _):
     #  случае у некоторых клиентов могут возникнуть проблемы.
     # смотри https://core.telegram.org/bots/api#callbackquery.
     query.answer()
-    # редактируем сообщение, тем самым кнопки
-    # в чате заменятся на этот ответ.
     query.edit_message_text(text=variant)
 
 
 
 def search_by_name(update, context):
-    d = ''.join(context.args)
+    d = context.user_data['dan']
     resp = req.get(
         f"https://www.googleapis.com/books/v1/volumes?q={d}:keyes&key=AIzaSyBW1ihw2fnM8jpQg1C-r77bAUYm-WhjJ20")
     z = resp.json()['items']
@@ -101,7 +102,31 @@ def search_by_name(update, context):
 
 
 def search_by_author(updatee, context):
-    pass
+    d = ''.join(context.args)
+    resp = req.get(
+        f"https://www.googleapis.com/books/v1/volumes?q={d}:keyes&key=AIzaSyBW1ihw2fnM8jpQg1C-r77bAUYm-WhjJ20")
+    z = resp.json()['items']
+    print(resp.json())
+    f = 0
+    for el in z:
+        d = []
+        f = 0
+        x = el['volumeInfo']
+        print(x)
+        for k, v in x.items():
+            f += 1
+            if f <= 5:
+                d.append(str(str(k) + ': ' + str(v)))
+        update.message.reply_text('\n'.join(d))
+        context.bot.send_photo(
+            update.message.chat_id,  # Идентификатор чата. Куда посылать картинку.
+            # Ссылка на static API, по сути, ссылка на картинку.
+            # Телеграму можно передать прямо её, не скачивая предварительно карту.
+            x['imageLinks']['smallThumbnail'],
+            caption="Нашёл:"
+        )
+
+        update.message.reply_text(x['averageRating'])
 
 
 def work_time(update, context):
@@ -126,6 +151,7 @@ def main():
     dp.add_handler(CommandHandler("close", close_keyboard))
     dp.add_handler(CommandHandler("address", address))
     dp.add_handler(CommandHandler("phone", phone))
+
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("site", site))
     dp.add_handler(CommandHandler("test", test))
@@ -133,6 +159,7 @@ def main():
     dp.add_handler(CommandHandler("search_by_author", search_by_author))
     dp.add_handler(CommandHandler("search_by_name", search_by_name))
     dp.add_handler(CommandHandler("work_time", work_time))
+    dp.add_handler(MessageHandler(Filters.text, 'param', pass_user_data=True))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
     dp.add_handler(CommandHandler("help", help))
     updater.start_polling()
