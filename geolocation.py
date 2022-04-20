a1a7711d5d8523d3d1d178d49e401e67317da654
Professional <MaRia.KuRganskaya5@yandex.ru>
2022-04-20 22:43:09 +0300

    iiiiii

---
+++ geolocation.py
@@ -0,0 +1,11 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'geoloctaion'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
